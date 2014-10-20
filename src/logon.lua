6adbf02c72f4173a40cffafab8e4f57ca7faa739
Yonaba <roland.yonaba@gmail.com>
2014-10-20 21:17:26 +0000

    Tabs to 2spaces

--- src/logon.lua
+++ src/logon.lua
@@ -7,9 +7,9 @@ font:setPixelSizes(0,13)
 
 --Functions
 function printCenteredfont(y,text,color) 
-	local length = string.len(text) 
-	local x = 240 - ((length*8)/2) 
-	screen:fontPrint(font,x,y,text,color) 
+  local length = string.len(text) 
+  local x = 240 - ((length*8)/2) 
+  screen:fontPrint(font,x,y,text,color) 
 end 
 -----------
 
@@ -17,30 +17,30 @@ nickname = System.nickName()
 cursor = Image.load("IMG/cursor.png")
 
 cursor = {
-	x = 200;
-	y = 200;
-	img = cursor;
+  x = 200;
+  y = 200;
+  img = cursor;
 }
 
 profile_pic = Image.load("IMG/profile_pic.png")
 
 while true do
-	screen:clear(Color.new(255,180,12))
-	pad = Controls.read()
+  screen:clear(Color.new(255,180,12))
+  pad = Controls.read()
 
-	if pad:analogX() < -50 then cursor.x = cursor.x - 6 end
-	if pad:analogY() < -50 then cursor.y = cursor.y - 6 end
-	if pad:analogX() > 50 then cursor.x = cursor.x + 6 end
-	if pad:analogY() > 50 then cursor.y = cursor.y + 6 end
+  if pad:analogX() < -50 then cursor.x = cursor.x - 6 end
+  if pad:analogY() < -50 then cursor.y = cursor.y - 6 end
+  if pad:analogX() > 50 then cursor.x = cursor.x + 6 end
+  if pad:analogY() > 50 then cursor.y = cursor.y + 6 end
 
-	if pad:cross() and cursor.x >= 194 and cursor.x <= 286 and cursor.y >= 99 and cursor.y <= 173 then 
-		dofile("sound_boot.lua")
-		dofile("ramclean.lua")
-	end
+  if pad:cross() and cursor.x >= 194 and cursor.x <= 286 and cursor.y >= 99 and cursor.y <= 173 then 
+    dofile("sound_boot.lua")
+    dofile("ramclean.lua")
+  end
 
-	screen:blit(194,99,profile_pic)
-	screen:blit(cursor.x,cursor.y,cursor.img)
+  screen:blit(194,99,profile_pic)
+  screen:blit(cursor.x,cursor.y,cursor.img)
 
-	screen.waitVblankStart()
-	screen.flip()
+  screen.waitVblankStart()
+  screen.flip()
 end
--- src/mp3.lua
+++ src/mp3.lua
@@ -5,23 +5,23 @@ font = Font.createProportional()
 font:setPixelSizes(0,18)
 
 function printCenteredfont(y,text,color) 
-	local length = string.len(text) 
-	local x = 240 - ((length*8)/2) 
-	screen:fontPrint(font,x,y,text,color) 
+  local length = string.len(text) 
+  local x = 240 - ((length*8)/2) 
+  screen:fontPrint(font,x,y,text,color) 
 end 
 
 while true do
-	screen:clear()
-	timer = timer + 1
+  screen:clear()
+  timer = timer + 1
 
-	screen:blit(0,0,icon)
-	printCenteredfont(220,"Chargement de Lua System Mp3 Player...", Color.new(0,255,0))
+  screen:blit(0,0,icon)
+  printCenteredfont(220,"Chargement de Lua System Mp3 Player...", Color.new(0,255,0))
 
-	if timer >= 40 then 
-		collectgarbage()
-		dofile("loadmp3.lua")
-	end
+  if timer >= 40 then 
+    collectgarbage()
+    dofile("loadmp3.lua")
+  end
 
-	screen.waitVblankStart()
-	screen.flip()
+  screen.waitVblankStart()
+  screen.flip()
 end
